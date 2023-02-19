c83e335bcf85c3cd1af7a8ec4fd2901620a653f4
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2023-02-19 20:39:07 +0530

    [Sangmeshwar] Refactor: Created An Array For Employee Wage for Multiple Company

--- EmployeeWageCalculation/EmployeeWageCalculation/Program.cs
+++ EmployeeWageCalculation/EmployeeWageCalculation/Program.cs
@@ -1,4 +1,4 @@
-﻿using EmployeeWage;
+﻿using EmployeeWageCalculation;
 using System;
 
 namespace EmployeeWageCalculation
@@ -8,12 +8,10 @@ namespace EmployeeWageCalculation
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee wage Calculation ");
-            EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 10, 200);
-            EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 15, 20, 100);
-            dMart.computeEmpWage();
-            Console.WriteLine(dMart.toString());
-            reliance.computeEmpWage();
-            Console.WriteLine(reliance.toString());
+            EmpWageBuilderObject empWageBuilderObject = new EmpWageBuilderObject();
+            empWageBuilderObject.AddCompanyEmpWage("DMart", 20, 15, 100);
+            empWageBuilderObject.AddCompanyEmpWage("Reliance", 10, 20, 200);
+            empWageBuilderObject.ComputeEmpWage();
         }
     }
 }
