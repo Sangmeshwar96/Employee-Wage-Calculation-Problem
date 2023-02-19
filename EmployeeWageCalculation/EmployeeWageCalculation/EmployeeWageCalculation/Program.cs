e8c365a9d2e4a684c42969d9cc4d8f234f41e31c
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2023-02-19 19:53:09 +0530

    [Sangmeshwar] Add: Calculating Total employee Wage by Company

---
+++ EmployeeWageCalculation/EmployeeWageCalculation/EmployeeWageCalculation/Program.cs
@@ -0,0 +1,21 @@
+﻿using EmployeeWage;
+using System;
+
+namespace EmployeeWageCalculation
+{
+    //Already Using class
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee wage Calculation ");
+            Emp emp1 = new Emp();
+            emp1.Wage("TCS", 20, 20, 100);
+            Emp emp2 = new Emp();
+            emp2.Wage("Wipro", 30, 20, 100);
+            Console.WriteLine("\nTotal wage for an employee of TCS is "+emp1.totalWage);
+            Console.WriteLine($"\nTotal wage for an employee of Wipro is "+emp2.totalWage);
+        }
+    }
+}
